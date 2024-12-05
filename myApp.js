fd7ed0615449658be1f9b8e434736850c5d13560
Nathaniel Ahy <nathanielaahy@hotmail.com>
2024-12-05 08:21:54 +0000

    "Committed!"

--- myApp.js
+++ myApp.js
@@ -3,9 +3,10 @@ let app = express();
 
 console.log("Hello World");
 
-app.get("/", (req, res)=>{
-    res.send("Hello Express");
-})
+app.get("/", function(req, res) {
+    res.send('Hello Express');
+  }
+);
 
 
 
